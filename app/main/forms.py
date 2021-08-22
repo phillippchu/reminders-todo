b5ed9b288e16725febec9630f1a0fe9d547c8515
Phillip Chu <misterpchu@gmail.com>
2021-08-22 14:09:12 -0700

    Task form description can be nullable

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,6 +5,6 @@ from wtforms.validators import DataRequired
 
 class TaskForm(FlaskForm):
     title = StringField("Title:", validators=[DataRequired()])
-    description = TextAreaField("Description:", validators=[DataRequired()])
+    description = TextAreaField("Description:")
     complete = BooleanField("Complete:")
     submit = SubmitField("Submit")
